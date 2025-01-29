0b61ebed7ff320232dfa20a35bbf3280a8766d8c
Botashka-Nur <nurkasyvomabota@gmail.com>
2025-01-29 14:11:19 +0500

    lab 3

--- function.py
+++ function.py
@@ -40,6 +40,71 @@ def my_function(fruit3, fruit2, fruit1):
 my_function(fruit1 = "Yubari King Melon", fruit2 = "Densuke Watermelon", fruit3 = "Taiyo no Tamago Mango")
 
 
+#Arbitrary Keyword Arguments, **kwargs
+def my_function(**sb):
+  print("My first subject " + sb["lname"])
+
+my_function(fname = "Calculus 1", lname = "PP1")
 
 
+#Default Parameter Value
+def my_function(country = "Norway"):
+  print("I am from " + country)
 
+my_function("Sweden")
+my_function("KZ")
+my_function()
+my_function("Argentina")
+
+
+#Passing a List as an Argument
+def my_function(celebration):
+  for x in celebration:
+    print(x)
+
+celebration = ["birthday", "Nauryz", "New year"]
+
+my_function(celebration)
+
+
+#Return Values
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+#Positional-Only Arguments
+def my_function(x, /):
+  print(x)
+
+my_function(3)
+
+
+#Keyword-Only Arguments
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3)
+
+
+#Combine Positional-Only and Keyword-Only
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
+
+
+#Recursion
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
